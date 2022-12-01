3a9e5c923d3a9808a9ec209ce4646e55b718f275
aylancOnur <aylanc.onur@gmail.com>
2022-12-01 15:43:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Keep Buy</Text>
+      <Icon name="rocket" size={30} color="#900" />
+    </View>
+  );
+};
+
+export {App};
